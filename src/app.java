d419289ece154532b4ac90fb66dcb228142381b3
jasper <whiteana001@warren.k12.in.us>
2024-09-20 13:25:56 -0400

    initial commit

--- src/app.java
+++ src/app.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class app {
+import java.util.Random;
+
+public class app {
+    public static void main(String[] args) {
+        rollSixDice();
+    }
+
+
+    private static void rollSixDice() {
+        Random rand = new Random();
+        for (int i = 0; i < 6; i++){
+            int roll = rand.nextInt(6);
+            roll++;
+            System.out.println(roll);
+        }
+
+    }
 }
